d0c089fad17fb372ed6150d57d0daf02c0302216
Іван Пінчук <vanilla.com.ua@gmail.com>
2021-05-19 01:04:29 +0300

    111

---
+++ BIM Leaders/BIM Leaders Resources/ResourceAssembly.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace BIM_Leaders_Resources
+{
+    public static class ResourceAssembly
+    {
+        public static Assembly GetAssembly()
+        {
+            return Assembly.GetExecutingAssembly();
+        }
+        public static string GetNamespace()
+        {
+            return typeof(ResourceAssembly).Namespace + ".";
+        }
+    }
+}
---
+++ BIM Leaders/Forms/Dimension_Section_Floors_Form.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _BIM_Leaders.Forms
+{
+    /// <summary>
+    /// Interaction logic for Dimension_Section_Floors_Form.xaml
+    /// </summary>
+    public partial class Dimension_Section_Floors_Form : UserControl
+    {
+        public Dimension_Section_Floors_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
