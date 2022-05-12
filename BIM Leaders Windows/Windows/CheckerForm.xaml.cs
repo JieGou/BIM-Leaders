cba47e791f2eb9fbcf9a7ba6e573e5de4ac31e50
Іван Пінчук <vanilla.com.ua@gmail.com>
2022-05-12 16:18:41 +0300

    Select all button - in Checker form.

--- BIM Leaders Windows/Windows/CheckerForm.xaml.cs
+++ BIM Leaders Windows/Windows/CheckerForm.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using MaterialDesignColors;
 using MaterialDesignThemes.Wpf;
+using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 
@@ -32,6 +34,21 @@ namespace BIM_Leaders_Windows
             // are searched relative to Eclipse's path, so they're not found.
             Card card = new Card();
             Hue hue = new Hue("Dummy", Colors.Black, Colors.White);
+        }
+
+        // Select all categories if "Select all" label was clicked.
+        private void SelectAll_Click(object sender, RoutedEventArgs e)
+        {
+            ObservableCollection<CheckBox> checkBoxList = new ObservableCollection<CheckBox>(){
+                checkBox00, checkBox01, checkBox02, checkBox03, checkBox04, checkBox05, checkBox06, checkBox07,
+                checkBox08, checkBox09, checkBox10, checkBox11, checkBox12, checkBox13, checkBox14, checkBox15,
+                checkBox16, checkBox17, checkBox18, checkBox19, checkBox20, checkBox21, checkBox22, checkBox23,
+            };
+
+            foreach (CheckBox c in checkBoxList)
+            {
+                c.IsChecked = true;
+            }
         }
 
         /// <summary>
