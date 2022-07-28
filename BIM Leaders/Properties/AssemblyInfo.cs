c8c3d0052db9010cca9ca187e0aa6112488d8ae6
Ivan Pinchuk <vanilla.com.ua@gmail.com>
2022-07-28 18:02:54 +0300

    Installer comments.

--- BIM Leaders/Properties/AssemblyInfo.cs
+++ BIM Leaders/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.22.7.20")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.22.7.28")]
